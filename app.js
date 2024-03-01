1e3160d2421191875191cef5af1589ffc482def9
saadmaikaho <saadghazalisalisu@gmail.com>
2024-03-01 13:49:52 +0800

    commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 // Enable CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'tkcompany.vercel.app', 'tradegiftcard.net'); // Update the allowed origin
+  res.header('Access-Control-Allow-Origin', '*'); // Update the allowed origin
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Credentials', true);
